d0f5cc2d9e595af7a9535c1bb437bec3b5735269
duc-png <duc17012003@gmail.com>
2025-06-12 16:22:50 +0700

    Initial commit

---
+++ src/main/java/com/example/shopeefood/ShopeeFooodApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopeefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeFooodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeFooodApplication.class, args);
+	}
+
+}
